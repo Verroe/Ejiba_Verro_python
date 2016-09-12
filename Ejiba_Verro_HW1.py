0c94d70b5c7816e0dec2a1c02d1e4681f56ac55f
Verro <vme8@georgetown.edu>
2016-09-12 19:35:53 -0400

    Solution to HW 1 with Problem 7 added and 5 revised

--- Ejiba_Verro_HW1.py
+++ Ejiba_Verro_HW1.py
@@ -4,18 +4,17 @@ Created on Thu Sep  8 19:25:15 2016
 
 @author: verroejiba
 """
+#Verro Ejiba Group A Homework 1 Due 9/15/2016
 import os 
-import linecache
-print(os.getcwd())
-os.chdir(input('Enter new directory: '))
+import io
+print(os.getcwd()) #Check the working directory 
+os.chdir(input('Enter new directory: ')) #Change to the desired directory
 
 #Solution to number 1
 #A function to open and read the file to read to be checked for palindrome
 def openFile(filename):
     file = open('filename.txt')
-    file.read()
-    file.split()
-    #file.write()
+    file.read().split()
     
 #Input your file name
 openFile(input('Enter file name: '))
@@ -23,7 +22,7 @@ openFile(input('Enter file name: '))
 for word in openFile(input('Enter file name: ')):
     #check for palindrome
         word = "radar"
-        reverse = word[::-1]
+        reverse = word[::-1] #write the words backward
         if word == reverse:
             print(word, 'is a palindrome')
         else:
@@ -34,8 +33,9 @@ for word in openFile(input('Enter file name: ')):
 #A function that find all the hapaxes
 #given the following text check for the word that appears once
 sent = "Life is beautiful and it is life" 
-locase = sent.lower() #writes all the uppercase letters in lowercase
-wlist = locase.split() #Separates the sentence in words
+#Changes all capital letters to lowercase and
+#Separates the sentence in words
+wlist = sent.lower().split() 
 newlist = set() #A new set to store words one time
 rlist = set() #A set to store all the words that have been removed
 #def hapax(openFile(input('Enter file name: ')))
@@ -56,11 +56,29 @@ example = '''Love your neighbor as you love yourself
 #open the desired file
 file = openFile(input('Enter file name: ')) 
 #make copy from the existing one
-nfile = file.copy()   
-for line in nfile:
-    enumerate(line)
-print(nfile)
+nfile = file.copy() 
+nlines = len(io.readline(example))  #Get the number of lines
+for line in example:
+    enumerate(line, start=1)
+print(nlines)
+
+#Solution to number 7: Average word length of a text
+
+avgFile = openFile(input('Enter file naem: '))
+sep = avgFile.split()#splits the words in order to ignore spaces
+newset = set() #Created set that will take all the non repeated words not
+total = len(sep) #Count the total number of words 
+count = 0
+for word in sep: 
+    if word not in newset: #counts and adds the word not in the new set
+        count += len(word)
+        newset.add(word)
+    else: #counts the repeated words
+        count += len(word)
+        
+avg = (count)/total # calculates the average word length
     
+print(count, total, avg)
     
 
 
