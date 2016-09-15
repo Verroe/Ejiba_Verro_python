6706a609d6836be5db0776488790132c5f36a971
Verroe <vme8@georgetown.edu>
2016-09-15 13:02:45 -0400

    Update HW1_7.py

--- HW1_7.py
+++ HW1_7.py
@@ -16,30 +16,27 @@ os.chdir(input('Enter new directory: ')) #Change to the desired directory
 def openFile(filename):
     file = open('filename.txt')
     file.read().split()
-    
-#Input your file name
-openFile(input('Enter file name: '))
-
 
 '''Solution to number 7: A program that calculates the average word length of 
         a text stored in a file '''
-
+#Open the desired file
 avgFile = openFile(input('Enter file name: '))
 #sep = avgFile.split()#splits the words in order to ignore spaces
 newset = set() #Created set that will take all the non repeated words not
 total = len(avgFile) #Count the total number of words 
 count = 0
-for word in avgFile: 
-    if word not in newset: #counts and adds the word not in the new set
-        count += len(word)
-        newset.add(word)
-    else: #counts the repeated words
-        count += len(word)
+def average(avgFile):
+    for word in avgFile: 
+        if word not in newset: #counts and adds the word not in the new set
+            count += len(word)
+            newset.add(word)
+        else: #counts the repeated words
+            count += len(word)
+            
+    avg = (count)/total # calculates the average word length
         
-avg = (count)/total # calculates the average word length
-    
-print(count, total, avg)
-    
+    print(count, total, avg)
+        
 
 
 
