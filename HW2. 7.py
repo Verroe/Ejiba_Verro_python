ac5b88bcc9b217103b26044926c72af46566a8d4
Verro <vme8@georgetown.edu>
2016-09-22 13:45:22 -0400

    HW problem 7

--- HW2. 7.py
+++ HW2. 7.py
@@ -7,7 +7,32 @@ Created on Thu Sep 22 11:48:35 2016
 ''' Write a function that accepts a list of strings and uses recursion to return a sorted list. Each
     step should modify at most two elements in the list
     Parameter =  a list of strings''' 
-def srt(lst):
-    lst =list(input("Enter a word:"))
+def srt(lst, offset = None):
+    #the case where sorting is insignificant
+    if len(lst) <2 :
+        print("There is not modification to make with the given input")
+    else:
+        if (offset is None):
+            offset = len(lst) - 1
+            
+        if (offset >= 0):
+            
+            lst = srt(lst, offset - 1)
+            elt = offset
+            
+            for r in range(offset+1, len(lst)):
+                if (lst[1] > lst[r]):
+                    elt = r
+                    
+            nelt = lst[offset]
+            lst[offset] = lst[elt]
+            lst[elt] = nelt
+            print(str(offset+1))
+            for r in range(0, len(lst)):
+                print(lst[r])
+        return lst
+        
     
+lst= [" I ","love", "God", "and", "my", "family"]
+srt(lst)
     
