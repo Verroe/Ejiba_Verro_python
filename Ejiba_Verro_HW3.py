c4cafefc7d1807fc2e6b43313c8dd248820f5e2e
Verro <vme8@georgetown.edu>
2016-10-01 13:43:04 -0400

    Update homework3

--- Ejiba_Verro_HW3.py
+++ Ejiba_Verro_HW3.py
@@ -12,40 +12,53 @@ that could hide the true shape of the distribution, separate that part of the da
  to plot a boxplot of the entire distribution.'''
 #Homework3
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 #import numpy as np
 
 abalone = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/abalone/abalone.data')
+abalone.columns =[]
 income = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data')
+income.colummns = []
 diamond = pd.read_csv('https://vincentarelbundock.github.io/Rdatasets/csv/Ecdat/Diamond.csv')
 diamond.columns=['Number','Carat','Colour','Clarity','Certification','Price']
-diamond.describe() #print the simple location statistics
-carat = diamond[['Carat']] #only print the "carat "column
-carat.hist()
-price = diamond[['Price']] #only print the "price" column
-price.hist()
-diamond.describe().loc[['count']]
 
-#a function that takes data as input, calculates the optimal bin size, separate the outliers and print plots for histograms  
+def optbin(file):
+    ''' a function that calculate the optimal bin size using the Freedman Diaconis rule
+    bin size = 2(iqr(x)/(n^1/3))
+    parameters:
+    file = the data to compute the bin size of
+    '''
+    loc_stat = file.describe() #gets the stat summary of the data
+    n = loc_stat.loc[['count']] #from summary get the size of the sample
+    Q1 = file.quantile(.25) #get the first quantile
+    Q3 = file.quantile(.75) #get the third quantile
+    IQR = Q3 - Q1  #compute the IQR range #not giving the right output, how can this be fixed?
+    bin_size = 2*(IQR/(n**(1/3))) #freedman rule 
+    return bin_size
+optbin(diamond)
+
 def dist(file):
+    '''A function that takes data as input, calculates the optimal bin size, 
+    separate the outliers and print plots for histograms 
+    file = data to work on 
+    '''
     #gets the simple location statistics
-    loc_stat = file.describe()
     for c in file.columns.values :
-        #print(file[c])
+        
+        bins = optbin(file)
+        Q1 = file.quantile(.25)
+        Q3 = file.quantile(.75)
             
-        Q1 = loc_stat.loc[['25%']]
-        Q3 = loc_stat.loc[['75%']]
-            
-        lower = file[c].transform(Q1 - (1.5*(Q3 - Q1))) #this line needs review
-        upper = file[c].transform(Q3 + (1.5*(Q3 - Q1)))#this line needs review
+        lower = file[c].where(Q1 - (1.5*(Q3 - Q1))) #this line needs review
+        upper = file[c].where(Q3 + (1.5*(Q3 - Q1)))#this line needs review
         
         outliers = (file[c] < lower) | (file[c] > upper)
         
-        if outliers == True:
-            outliers.hist() #plot the segment of data with outliers
+        if outliers.empty == False :
+            outliers.hist(file[c],bins) #plot the segment of data with outliers
         else:
             outliers.hist() #plot the segment of data wherever there is no outliers
-        file[c].boxplot() #plot the boxplot of the values of each column with numerical inputs'''
+        #file[c].boxplot() #plot the boxplot of the values of each column with numerical inputs'''
 
 dist(diamond)
         
